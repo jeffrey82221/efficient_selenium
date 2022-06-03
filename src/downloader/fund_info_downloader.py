7b4b46e6b2d55e16cd6f38ca2d897b2b8eea6828
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-03 15:33:08 +0000

    [feature] add fund info parallel extraction

---
+++ src/downloader/fund_info_downloader.py
@@ -0,0 +1,27 @@
+from src.processors.fund_info_extractor import FundDocumentPage, FundInfoExtractor
+class SerialFundInfoDownloader:
+    @staticmethod
+    def map(fund_link_generator):
+        for fund, url in fund_link_generator:
+            doc = FundInfoExtractor(FundDocumentPage().get_html(url))
+            yield fund, doc.extract_info()
+
+import ray
+from ray.util import ActorPool
+
+@ray.remote
+class FundInfoExtractActor:
+    def request(self, input_tuple):
+        fund, url = input_tuple
+        doc = FundInfoExtractor(FundDocumentPage().get_html(url))
+        return fund, doc.extract_info()
+
+class ParallelFundInfoDownloader:
+    def __init__(self, parallel_cnt):
+        self._pool = ActorPool([FundInfoExtractActor.remote() for i in range(parallel_cnt)])
+    
+    def map(self, fund_link_generator):
+        return self._pool.map(lambda actor, input_tuple: actor.request.remote(input_tuple), 
+            fund_link_generator)
+
+    
--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-from src.processors.fund_info_extractor import FundDocumentPage, FundInfoExtractor
-from src.downloader.fund_links_downloader import ParallelFundLinkDownloader
-
-downloader = ParallelFundLinkDownloader(100)
 import tqdm
-for fund_name, url in tqdm.tqdm(downloader.fund_link_generator(), total=downloader.fund_count):
-    doc = FundInfoExtractor(FundDocumentPage().get_html(url))
-    print(doc.isin)
-    print(doc.company)
+from src.downloader.fund_links_downloader import ParallelFundLinkDownloader
+from src.downloader.fund_info_downloader import ParallelFundInfoDownloader
+import pickle
+downloader = ParallelFundLinkDownloader(20)
+fund_links_path = 'data/fund_links.pickle'
+try:
+    fund_links = pickle.load(open(fund_links_path, 'rb'))
+except:
+    fund_links = list(tqdm.tqdm(downloader.fund_link_generator(), total=downloader.fund_count))
+    with open('data/fund_links.pickle', 'wb') as f:
+        pickle.dump(fund_links, f)
+print('fund_links Loaded')
+fund_info_downloader = ParallelFundInfoDownloader(5)
+for fund_name, info in tqdm.tqdm(
+    fund_info_downloader.map(fund_links), total=downloader.fund_count):
+    new = fund_name
+print('fund_info Loaded')

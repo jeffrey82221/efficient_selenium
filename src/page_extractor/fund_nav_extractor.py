19aad46358530faf2679acd7aefe55a13e80c784
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-04 09:04:20 +0000

    [refactor] move class to src

---
+++ src/page_extractor/fund_nav_extractor.py
@@ -0,0 +1,87 @@
+from src.base.driver_base import SeleniumBase
+from src.base.html_base import HtmlBase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import gc
+import pprint
+
+class _LastPageExtractor(HtmlBase):
+    def extract_info(self):
+        soup = self.soup
+        a_elements = soup.find_all('a')
+        page_buttoms = list(filter(
+            lambda a: a.has_attr('data-value'), a_elements))
+        page_count = int(page_buttoms[0].parent.parent.findChildren()[-1].text)
+        return page_count
+
+class NavView(SeleniumBase):
+    def __init__(self, verbose=False):
+        super().__init__()
+        self.__verbose = verbose
+    def initialize(self, url):
+        self.url = url
+        self.load_url(url)
+        self.current_page_index = 1
+        buttom_1 = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, "//a[@data-value='1']"))
+        )
+        self.current_page_buttom_class_name = buttom_1.get_attribute("class")
+        self.max_page_count = _LastPageExtractor(self.get_html()).extract_info()
+        gc.collect()
+            
+    def show_current_states(self):
+        pprint.pprint({
+            'url': self.url,
+            'max_page_count': self.max_page_count,
+            'current_page_buttom_class_name': self.current_page_buttom_class_name,
+            'current_page': self.current_page_index
+        })
+
+    def has_next_page(self):
+        return self.current_page_index < self.max_page_count
+
+    def goto_next_page(self):
+        assert self.current_page_index < self.max_page_count
+        self.make_action()
+        self.current_page_index += 1
+        self.wait_state_change(self.current_page_index)
+
+    def make_action(self):
+        buttom_name = '下一頁'
+        buttom = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, buttom_name))
+        )
+        assert buttom.is_enabled()
+        assert buttom.text == buttom_name
+        java_script = f"""
+        for (const a of document.querySelectorAll('a')) {{
+            if (a.textContent.includes('{buttom_name}')) {{
+                a.click()
+            }}
+        }}
+        """
+        self.driver.execute_script(java_script)
+    
+    def wait_state_change(self, page_index):
+        xpath_selector = f"//a[@class='{self.current_page_buttom_class_name}']"
+        WebDriverWait(self.driver, 20).until(
+            EC.text_to_be_present_in_element((
+                By.XPATH, 
+                xpath_selector
+            ), str(page_index))
+        )
+
+
+class NavExtractor(HtmlBase):
+    def extract_info(self):
+        soup = self.soup
+        tables = soup.find_all('table')
+        assert len(tables) == 2
+        nav_table = tables[1]
+        rows = nav_table.find_all('tr')[1:]
+        date_n_navs = list(map(self.extract_date_n_nav_from_row, rows))
+        return date_n_navs
+    def extract_date_n_nav_from_row(self, row):
+        cells = row.find_all('td')
+        return cells[0].get_text(), float(cells[1].get_text())

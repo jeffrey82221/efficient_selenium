ef439fc8a0a0660389cca0473e4518aea74febd8
jeffrey82221 <jeffrey82221@gmail.com>
2022-06-04 21:17:26 +0800

    [feature] selenium quit error handling

--- src/downloader/fund_nav_downloader.py
+++ src/downloader/fund_nav_downloader.py
@@ -11,8 +11,9 @@ import ray
 from ray.util import ActorPool
 import pprint
 import os
+import traceback
 VERBOSE = False
-TQDM_VERBOSE = True
+TQDM_VERBOSE = False
 @ray.remote
 class _FundNavExtractActor:
     def __init__(self):
@@ -21,33 +22,50 @@ class _FundNavExtractActor:
     def quit(self):
         self._nav_selenium.quit()
 
-    def request(self, input_tuple):
-        fund, url = input_tuple
-        self._nav_selenium.initialize(url)
-        info_extractor = FundInfoExtractor(FundDocumentPage().get_html(url))
-        isin = info_extractor.isin
-        company = info_extractor.company
+    def delete_table(self, company, isin):
         if os.path.exists(self.__get_table_path(company, isin)):
-            os.remote(self.__get_table_path(company, isin))
-        nav_segment_gen = self._nav_segment_generator()
-        if TQDM_VERBOSE:
-            for item in tqdm.tqdm(
-                map(
-                    partial(self._save_to_h5, isin=isin, company=company),
-                    nav_segment_gen
-                ),
-                desc=fund,
-                total=self._nav_selenium.max_page_count):
-                if VERBOSE:
-                    print(f"Finish Saving Data of {item} of {isin} of {company}")
-                else:
-                    continue
-        else:
-            _ = list(map(
-                    partial(self._save_to_h5, isin=isin, company=company),
-                    nav_segment_gen
-                ))
-        return f"NAV DOWNLOAD OF {fund}:{self.__get_table_path(company, isin)} COMPLETE"
+            if VERBOSE:
+                print(f'delete {self.__get_table_path(company, isin)}')
+            os.remove(self.__get_table_path(company, isin))
+
+    def request(self, input_tuple):
+        try:
+            fund, url = input_tuple
+            self._nav_selenium.initialize(url)
+            info_extractor = FundInfoExtractor(FundDocumentPage().get_html(url))
+            isin = info_extractor.isin
+            company = info_extractor.company
+            self.delete_table(company, isin)
+            nav_segment_gen = self._nav_segment_generator()
+            if TQDM_VERBOSE:
+                for item in tqdm.tqdm(
+                    map(
+                        partial(self._save_to_h5, isin=isin, company=company),
+                        nav_segment_gen
+                    ),
+                    desc=fund,
+                    total=self._nav_selenium.max_page_count):
+                    if VERBOSE:
+                        print(f"Finish Saving Data of {item} of {isin} of {company}")
+                    else:
+                        continue
+            else:
+                _ = list(map(
+                        partial(self._save_to_h5, isin=isin, company=company),
+                        nav_segment_gen
+                    ))
+            return f"NAV DOWNLOAD OF {fund}:{self.__get_table_path(company, isin)} COMPLETE"
+        except BaseException as e:
+            result = f"NAV DOWNLOAD FAILED:\n{fund}\nURL:\n{url}"
+            print(result)
+            traceback.print_exc()
+            self.quite()
+            print('Selenium driver stopped')
+            self.delete_table(company, isin)
+            print('Stored Table deleted')
+            self._nav_selenium = NavView()
+            print('Selenium driver restart')
+            return result
 
     def _nav_segment_generator(self):
         for i in range(self._nav_selenium.max_page_count):
@@ -85,8 +103,15 @@ class ParallelFundNavDownloader:
         self._pool = ActorPool(self._actors)
 
     def map(self, fund_link_generator):
-        return self._pool.map(lambda actor, input_tuple: actor.request.remote(input_tuple),
-                              fund_link_generator)
+        try:
+            return self._pool.map(lambda actor, input_tuple: actor.request.remote(input_tuple),
+                                fund_link_generator)
+        except:
+            self.quite()
+
     def quit(self):
         for actor in self._actors:
-            actor.quit.remote()
+            try:
+                actor.quit.remote()
+            except:
+                print(str(actor), 'selenium driver already quit')
